be0f663de8ed48e8ac0c47a5999f0cc70e6bae97
syi7190 <sungyi456@gmail.com>
2023-08-24 16:58:43 -0400

    a

--- APIs/api1_test.py
+++ APIs/api1_test.py
@@ -4,11 +4,11 @@ chat = []
 
 while True:
     prompt = input("You: ")
-    
+
     response = api1.Completion.create(
         prompt  = prompt,
         chat    = chat)
-    
+
     print("Bot:", response["response"])
-    
+
     chat.append({"question": prompt, "answer": response["response"]})
